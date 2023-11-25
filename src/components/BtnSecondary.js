469bec7ccd51c7de72e791605e6ee9201b7ecf81
ksc1728 <139704073+ksc1728@users.noreply.github.com>
2023-11-25 16:57:26 +0530

    Delete src directory

--- src/components/BtnSecondary.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BtnSecondary = (props) => {
-    return (
-        <button {...props} className='text-white bg-red-500 text-sm px-4 py-2 border border-red-500 rounded-md hover:bg-red-600/90 transition-colors focus:outline-none focus:ring focus:ring-red-300 focus:ring-offset-1'>{props.children}</button>
-    )
-}
-
-export default BtnSecondary
